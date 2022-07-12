15051968ab9a6879946cb0bb4a689b1e0dc26710
Frank Stallone III <frank.stallone@klaviyo.com>
2022-07-12 11:52:28 -0400

    refactor: ♻️ Lifting the app frame to root.jsx from page Routes

--- src/root.jsx
+++ src/root.jsx
@@ -3,12 +3,35 @@ import { Routes } from "solid-start/root";
 import { ErrorBoundary } from "solid-start/error-boundary";
 import { Suspense } from "solid-js";
 import "./index.css";
+import { For } from "solid-js";
+import * as mainNav from "./data/main-navigation";
+import { Link } from "solid-app-router";
+
 export default function Root() {
   return (
     <>
       <ErrorBoundary>
         <Suspense>
-          <Routes />
+          <main class="flex flex-row text-gray-700 h-max">
+            <div class="w-52 bg-slate-200">
+              <nav>
+                <ul className="list-none mx-2 text-sm">
+                  <For each={mainNav.default}>
+                    {(item, index) => (
+                      <li class="my-2">
+                        <Link href="/">
+                          {item.label}
+                        </Link>
+                      </li>
+                    )}
+                  </For>
+                </ul>
+              </nav>
+            </div>
+            <div class="w-full">
+              <Routes />
+            </div>
+          </main>
         </Suspense>
       </ErrorBoundary>
     </>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,26 +1,7 @@
 import { Link } from "solid-app-router";
-import { For } from "solid-js";
-import * as mainNav from "../data/main-navigation.json";
-
 export default function Home() {
   return (
-    <main class="flex flex-row text-gray-700 h-screen">
-      <div class="w-52 bg-slate-200">
-        <nav>
-          <ul className="list-none">
-            <For each={mainNav.default}>
-              {(item, index) => (
-                <li>
-                  <Link href="/">
-                    {item.label}
-                  </Link>
-                </li>
-              )}
-            </For>
-          </ul>
-        </nav>
-      </div>
-      <div class="w-full">
+    <>
         <p class="mt-8">
           Visit{" "}
           <Link
@@ -39,7 +20,6 @@ export default function Home() {
             About Page
           </Link>{" "}
         </p>
-      </div>
-    </main>
+    </>
   );
 }
