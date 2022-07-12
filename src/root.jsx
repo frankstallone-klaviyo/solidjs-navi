e4b669fd286276fe2a4b10c8b3f4d2b1817340ad
Frank Stallone III <frank.stallone@klaviyo.com>
2022-07-12 15:57:52 -0400

    refactor: ♻️  Overengineered: Using signals and states not context

--- src/root.jsx
+++ src/root.jsx
@@ -3,59 +3,48 @@ import { Routes } from "solid-start/root";
 import { ErrorBoundary } from "solid-start/error-boundary";
 import { Suspense } from "solid-js";
 import "./index.css";
-import { For, createContext, useContext } from "solid-js";
-import { createStore } from "solid-js/store";
+import { For, createSignal, Switch, Match } from "solid-js";
 import * as mainNav from "./data/main-navigation";
 import { NavLink } from "solid-app-router";
 
-export const NavigationContext = createContext([
-  "root",
-  {
-    changeActive: () => undefined,
-  }
-]);
-
-export function NavigationProvider(props) {
-  const [state, setState] = createStore({
-    active: props.active ?? "root",
-    menuItems: mainNav.default,
-  })
-
-  // const changeActive = (active) => setState("active", active);
-  const changeActive = (active) => setState("active", active);
-
-  return (
-    <NavigationContext.Provider value={[state, {changeActive}]}>
-      {props.children}
-    </NavigationContext.Provider>
-  )
-}
-
-export const useNav = () => useContext(NavigationContext);
 
 export default function Root() {
-  const [active, {changeActive}] = useNav();
-  console.log(active)
-  
+  const [active, setActive] = createSignal("Home");
+  console.log(mainNav.default)
+    
   return (
     <>
       <ErrorBoundary fallback={<div>Something went terribly wrong</div>}>
         <Suspense fallback={<div>Loading...</div>}>
-          <NavigationProvider>
             <main class="flex flex-row flex-0 text-gray-700 h-screen">
               <div class="w-52 bg-slate-200">
-              <button onClick={() => changeActive("new")}>{active}</button>
                 <nav>
                   <ul className="list-none mx-4 text-sm h-full">
-                    <For each={mainNav.default} fallback={<div>Loading...</div>}>
-                      {(item, index) => 
-                        <li class="my-3">
-                          <NavLink href={item.route}>
-                            {item.label}
-                          </NavLink>
-                        </li>
-                      }
-                    </For>
+                  <Switch fallback={<div>Not found</div>}>
+                    <Match when={active() === "Home" | "Flows"}>
+                      <For each={mainNav.default} fallback={<div>Loading...</div>}>
+                        {(item, index) => 
+                          <li class="my-3">
+                            <a href="#" onClick={() => setActive(item.label)}>
+                              {item.label}
+                            </a>
+                          </li>
+                        }
+                      </For>
+                    </Match>
+                    <Match when={active() === "Audience"}>
+                      <li class="my-3"><a href="#" onClick={() => setActive("Home")}>← Home</a></li>
+                      <For each={mainNav.default[1].children} fallback={<div>Loading...</div>}>
+                        {(item, index) => 
+                          <li class="my-3">
+                            <a href="#" onClick={() => setActive(item.label)}>
+                              {item.label}
+                            </a>
+                          </li>
+                        }
+                      </For>
+                    </Match>
+                  </Switch>
                   </ul>
                 </nav>
               </div>
@@ -63,7 +52,6 @@ export default function Root() {
                 <Routes />
               </div>
             </main>
-          </NavigationProvider>
         </Suspense>
       </ErrorBoundary>
     </>
