542dfc877890254cbda7a880eb213d4aa2f1eeb2
Frank Stallone III <frank.stallone@klaviyo.com>
2022-07-12 13:09:54 -0400

    content: 🍱 Small content changes

--- src/routes/flow.jsx
+++ src/routes/flow.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        About Page
+        Flow Page
       </h1>
       <Counter />
       <p class="mt-8">
